aaed3c78bea139ae92c7985427babd52ca6f2c2e
kyle <kyle@grafana.com>
2020-06-24 17:20:41 -0400

    some tests that pass

--- pkg/tsdb/azuremonitor/applicationinsights-metrics.go
+++ pkg/tsdb/azuremonitor/applicationinsights-metrics.go
@@ -1,6 +1,118 @@
 package azuremonitor
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+)
+
+func (mr *MetricsResult) ToFrame(metric, agg string, dimensions []string) (*data.Frame, error) {
+	dimLen := len(dimensions)
+	frame := data.NewFrame("", data.NewField("StartTime", nil, []time.Time{}))
+
+	fieldIdxMap := map[string]int{}
+
+	rowCounter := 0
+
+	for _, seg := range *mr.Value.Segments {
+		labels := data.Labels{}
+
+		handleInnerSegment := func(s MetricsSegmentInfo) error {
+			met, ok := s.AdditionalProperties[metric]
+			if !ok {
+				return fmt.Errorf("expected additional properties not found on inner segment while handling azure query")
+			}
+			metMap, ok := met.(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("unexpected type for additional properties not found on inner segment while handling azure query")
+			}
+			metVal, ok := metMap[agg]
+			if !ok {
+				return fmt.Errorf("expected aggregation value for aggregation %v not found on inner segment while handling azure query", agg)
+			}
+			if dimLen != 0 {
+				key := dimensions[len(dimensions)-1]
+				val, ok := s.AdditionalProperties[key]
+				if !ok {
+					return fmt.Errorf("unexpected dimension/segment key %v not found in response", key)
+				}
+				sVal, ok := val.(string)
+				if !ok {
+					return fmt.Errorf("unexpected dimension/segment value for key %v in response", key)
+				}
+				labels[key] = sVal
+			}
+
+			if _, ok := fieldIdxMap[labels.String()]; !ok {
+				frame.Fields = append(frame.Fields, data.NewField(metric, labels.Copy(), make([]*float64, 1)))
+				fieldIdxMap[labels.String()] = len(frame.Fields) - 1
+			}
+			var v *float64
+			if val, ok := metVal.(float64); ok {
+				v = &val
+			}
+			frame.Set(fieldIdxMap[labels.String()], rowCounter, v)
+
+			return nil
+		}
+
+		// Simple case with no Segments/Dimensions
+		if len(dimensions) == 0 {
+			frame.Extend(1)
+			frame.Set(0, rowCounter, seg.Start)
+			err := handleInnerSegment(seg)
+			rowCounter++
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		// Case with Segments/Dimensions
+		next := &seg
+		// decend (fast forward) to the next nested MetricsSegmentInfo by moving the 'next' pointer
+		decend := func(dim string) error {
+			if next == nil || next.Segments == nil || len(*next.Segments) == 0 {
+				return fmt.Errorf("unexpected insights response while handling dimension %s", dim)
+			}
+			next = &(*next.Segments)[0]
+			return nil
+		}
+		if dimLen > 1 {
+			if err := decend("root-level"); err != nil {
+				return nil, err
+			}
+		}
+		// When multiple dimensions are requests, there are nested MetricsSegmentInfo objects
+		// The higher levels just contain all the dimension key-value pairs except the last.
+		// So we fast forward to the depth that has the last tag pair and the metric values
+		// collect tags along the way
+		for i := 0; i < dimLen-1; i++ {
+			segStr := dimensions[i]
+			labels[segStr] = next.AdditionalProperties[segStr].(string)
+			if i != dimLen-2 { // the last dimension/segment will be in same []MetricsSegmentInfo slice as the metric value
+				if err := decend(string(dimensions[i])); err != nil {
+					return nil, err
+				}
+			}
+		}
+		if next == nil {
+			return nil, fmt.Errorf("unexpected dimension in insights response")
+		}
+		frame.Extend(1)
+		frame.Set(0, rowCounter, seg.Start)
+		for _, innerSeg := range *next.Segments {
+			err := handleInnerSegment(innerSeg)
+			if err != nil {
+				return nil, err
+			}
+		}
+		rowCounter++
+	}
+	return frame, nil
+}
 
 // MetricsResult a metric result.
 type MetricsResult struct {
@@ -12,9 +124,9 @@ type MetricsResultInfo struct {
 	// AdditionalProperties - Unmatched properties from the message are deserialized this collection
 	AdditionalProperties map[string]interface{} `json:""`
 	// Start - Start time of the metric.
-	Start string `json:"start,omitempty"`
+	Start time.Time `json:"start,omitempty"`
 	// End - Start time of the metric.
-	End string `json:"end,omitempty"`
+	End time.Time `json:"end,omitempty"`
 	// Interval - The interval used to segment the metric data.
 	Interval *string `json:"interval,omitempty"`
 	// Segments - Segmented metric data (if segmented).
@@ -26,9 +138,9 @@ type MetricsSegmentInfo struct {
 	// AdditionalProperties - Unmatched properties from the message are deserialized this collection
 	AdditionalProperties map[string]interface{} `json:""`
 	// Start - Start time of the metric segment (only when an interval was specified).
-	Start string `json:"start,omitempty"`
+	Start time.Time `json:"start,omitempty"`
 	// End - Start time of the metric segment (only when an interval was specified).
-	End string `json:"end,omitempty"`
+	End time.Time `json:"end,omitempty"`
 	// Segments - Segmented metric data (if further segmented).
 	Segments *[]MetricsSegmentInfo `json:"segments,omitempty"`
 }
@@ -56,7 +168,7 @@ func (mri *MetricsSegmentInfo) UnmarshalJSON(body []byte) error {
 			}
 		case "start":
 			if v != nil {
-				var start string
+				var start time.Time
 				err = json.Unmarshal(*v, &start)
 				if err != nil {
 					return err
@@ -65,7 +177,7 @@ func (mri *MetricsSegmentInfo) UnmarshalJSON(body []byte) error {
 			}
 		case "end":
 			if v != nil {
-				var end string
+				var end time.Time
 				err = json.Unmarshal(*v, &end)
 				if err != nil {
 					return err
@@ -110,7 +222,7 @@ func (mri *MetricsResultInfo) UnmarshalJSON(body []byte) error {
 			}
 		case "start":
 			if v != nil {
-				var start string
+				var start time.Time
 				err = json.Unmarshal(*v, &start)
 				if err != nil {
 					return err
@@ -119,7 +231,7 @@ func (mri *MetricsResultInfo) UnmarshalJSON(body []byte) error {
 			}
 		case "end":
 			if v != nil {
-				var end string
+				var end time.Time
 				err = json.Unmarshal(*v, &end)
 				if err != nil {
 					return err
